9a4f63284b44fe2134b1d1f4b297b72eb3a23559
Jayaraju Metta <jayaraju.metta@qualitestgroup.com>
2022-09-27 11:55:54 +0530

    firstcommit

---
+++ src/test/java/UITestSuite.java
@@ -0,0 +1,14 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class UITestSuite {
+
+    @Test
+    public void navigateToAmazon(){
+        System.setProperty("webdriver.chrome.driver","src/main/resources/drivers/chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.google.com");
+        driver.close();
+    }
+}
